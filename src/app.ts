7d596a77a9c87f5e9e89a6d53d73c6d30955d617
Andre Arahawa <andre.arahawa@ampli.com.br>
2021-10-09 19:58:19 -0300

    feat(backend): add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(express.json());
 app.use(router);
+app.use(cors(corsOptions));
 
 export { app };
