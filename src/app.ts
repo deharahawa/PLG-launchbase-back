1bd49b4c5824236446df5ba1a8dc60fc06c6ce52
Andre Arahawa <andre.arahawa@ampli.com.br>
2021-10-10 13:10:25 -0300

    feat(backend): add http proxy

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { router } from "./routes";
 import path from "path";
+import httpProxy from "http-proxy";
 
 const app = express();
 
@@ -10,6 +11,13 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+
+httpProxy.createProxyServer({
+  target: process.env.PROXY_SERVER,
+  toProxy: true,
+  changeOrigin: true,
+  xfwd: true,
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
